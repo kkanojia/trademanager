d0da327c755c859c003b60ee18f228d4969f07f9
Kunal Kanojia <kanojia.kunal@yahoo.com>
2015-09-03 02:27:26 +0530

    Date format fix

--- src/main/webapp/js/transactionscontroller.js
+++ src/main/webapp/js/transactionscontroller.js
@@ -49,7 +49,7 @@ function CreateTransactionController($scope, $location, TradeManagerService, $ro
 		$rootScope.infoMessage = "";
 		$rootScope.error = "";
 		$scope.tradeEntry.assetId = $scope.tradeEntry.assetinfo.id;
-		$scope.tradeEntry.tradeDate = $scope.tradeEntry.tradeDate.toISOString().substring(0, 10);
+		$scope.tradeEntry.tradeDate = $scope.format($scope.tradeEntry.tradeDate, 'dd-MMM-yyyy');
 
 		$scope.tradeEntry.$save(function(responseEntry) {
 			if (responseEntry.id == undefined) {
@@ -57,6 +57,24 @@ function CreateTransactionController($scope, $location, TradeManagerService, $ro
 			}
 			$location.path('/transactions/index');
 		});
+	};
+
+	//fix for timezone
+	$scope.format = function date2str(x, y) {
+	    var z = {
+	        M: x.getMonth() + 1,
+	        d: x.getDate(),
+	        h: x.getHours(),
+	        m: x.getMinutes(),
+	        s: x.getSeconds()
+	    };
+	    y = y.replace(/(M+|d+|h+|m+|s+)/g, function(v) {
+	        return ((v.length > 1 ? "0" : "") + eval('z.' + v.slice(-1))).slice(-2)
+	    });
+
+	    return y.replace(/(y+)/g, function(v) {
+	        return x.getFullYear().toString().slice(-v.length)
+	    });
 	};
 
 };
